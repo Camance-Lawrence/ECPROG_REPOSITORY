c5f37caa7ca5c3447d7c0a3325d9c553b31b6ec3
CamanceLawrence <lawrence.camance.23@gmail.com>
2026-07-08 10:21:56 +0800

    EMPTY SHELL

--- Challenge 1 Dr Santos Student Registry/Student_Registry_STRUCT.c
+++ Challenge 1 Dr Santos Student Registry/Student_Registry_STRUCT.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 
@@ -7,35 +8,81 @@
 
 typedef struct{
     int idnum;
-    int name[MAXLETTERS];
-    int major[MAXLETTERS];
+    char name[MAXLETTERS];
+    char major[MAXLETTERS];
     float gpa;
     int credits;
 }STUDENT;
 
-void menu(int *men1, int *men2){
-    printf("======================\nStudent Registry Menu:\n======================\n");
-    printf("Press[1]:ADD NEW STUDENT\nPress[2]:SEARCH STUDENT\nPress[3]:DISPLAY ALL STUDENTS\nPress[0] EXIT PROGRAM\n\nEnter Choice:");
+int numcheck(int *num);
 
-}
 
 int main(){
 
     STUDENT student[MAXSTUDENTS];
+    int men1=0;
+    char choice;
+    do{
+        int flag1=0;
+        while(flag1==0){
+            int men1=0;
+            system("cls");
+            printf("======================\nStudent Registry Menu:\n======================\n");
+            printf("Press[1]:ADD NEW STUDENT\nPress[2]:SEARCH STUDENT\nPress[3]:DISPLAY ALL STUDENTS\nPress[0] EXIT PROGRAM\n\nEnter Choice:");
 
-    int restart = 0;
+            int flag2=0;
+            if(numcheck(&men1)){
+                if(men1 < 0 || men1 > 3)
+                {
+                    printf("PLEASE SELECT FROM NUMBER 0-3 only\npress enter to continue:\n");
+                    getchar();getchar();
+                }
 
-    while(!restart){
-        int men1 = 0;
-        int men2 = 0;
+                else{
+                    flag1=1;
+                }
 
-        menu(&men1, &men2);
+            }
 
-    }
+            else{
+                printf("ENTER A NUMBER ONLY!\npress enter to continue:\n");
+                getchar();
+
+            }
+        }
+
+
+        if(men1 != 0){
+            printf("Return to Main Menu?(y/n):");
+            scanf(" %c", &choice);           
+        }
+
+
+    }while(choice == 'y' || choice =='Y');
 
     
     
     
 
     return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//FUNCTIONS:
+
+int numcheck(int *num){
+    if(scanf("%d", num)==1){
+        return 1;
+    }
+
+    else{
+        while(getchar() !='\n');
+        return 0;
+    }
+
 }
